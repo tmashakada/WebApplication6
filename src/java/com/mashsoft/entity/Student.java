37e26494c9f2d4331d670b6e2f0a801cf1a4fed3
tmash <tmashakada10@gmail.com>
2020-08-06 10:35:28 +0200

    my new project git

---
+++ src/java/com/mashsoft/entity/Student.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mashsoft.entity;
+
+/**
+ *
+ * @author Software Engineer tmashakada email:tmashakada10@gmail.com
+ */
+public class Student {
+    
+    private String studentnumber;
+    private String firstname;
+
+    public Student() {
+    }
+
+    public String getStudentnumber() {
+        return studentnumber;
+    }
+
+    public void setStudentnumber(String studentnumber) {
+        this.studentnumber = studentnumber;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+    
+    
+}
